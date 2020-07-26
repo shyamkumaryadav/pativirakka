a930b686371741c138b1ce1a5ba6c405fa71daa7
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-07-27 00:56:28 +0530

    git remote add origin git@github.com:shyamkumaryadav/pativirakka.git

---
+++ pativirakka/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from twilio.rest import Client
+from twilio.twiml.messaging_response import MessagingResponse, Body, Message, Redirect, Media
+
+@csrf_exempt
+def home(request):
+    if request.method == "POST":
+        print(request.POST)
+        msg = request.POST.get('Body')
+        phone = request.POST.get('From')
+        response = MessagingResponse()
+        message = Message()
+        if int(request.POST.get('NumMedia')) != 0:
+            message.media(url="https://demo.twilio.com/owl.png")
+        if msg != '':
+            message.body(f'{phone} said:\n{msg}')
+        response.append(message)
+        return HttpResponse(str(response))
+    return HttpResponse("<h1>Hello Ji</h1>")
