ef251e30fa9621ca6238c2236f2cc48921e33b5b
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-07-28 03:16:35 +0530

    False

--- pativirakka/settings.py
+++ pativirakka/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".ngrok.io", ".herokuapps.com", "localhost", "127.0.0.1"]
 
