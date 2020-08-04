617eb56d7f1ce1ee9e29d2ba21073bc5d27c7431
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-08-05 00:43:55 +0530

    Self

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py makemigrations && python manage.py migratemakemigrations
+release: python manage.py makemigrations && python manage.py migrate
 web: gunicorn pativirakka.wsgi —-log-file -
--- pativirakka/test.py
+++ pativirakka/test.py
@@ -1,10 +1,10 @@
 from django.test import TestCase
 
+
 class ViewTest(TestCase):
 
-    def test_is_false(Self):
+    def test_is_false(self):
         self.assertFalse(False)
-
 
     def test_home_page(self):
         response = self.client.get('/')
