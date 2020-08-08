488a4ed46b1c407322b5f1c878d4a7aad007e07c
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-08-08 16:45:45 +0530

    Bug 1010

--- pativirakka/settings/production.py
+++ pativirakka/settings/production.py
@@ -5,6 +5,55 @@ import django_heroku
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = False
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '[contactor] %(levelname)s %(asctime)s %(message)s'
+        },
+    },
+    'handlers': {
+        # Send all messages to console
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+        # Send info messages to syslog
+        # 'syslog':{
+        #     'level':'INFO',
+        #     'class': 'logging.handlers.SysLogHandler',
+        #     'facility': SysLogHandler.LOG_LOCAL2,
+        #     'address': '/dev/log',
+        #     'formatter': 'verbose',
+        # },
+        # Warning messages are sent to admin emails
+        # 'mail_admins': {
+        #     'level': 'WARNING',
+        #     'filters': ['require_debug_false'],
+        #     'class': 'django.utils.log.AdminEmailHandler',
+        # },
+        # critical errors are logged to sentry
+        'sentry': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'raven.contrib.django.handlers.SentryHandler',
+        },
+    },
+    'loggers': {
+        # This is the "catch all" logger
+        '': {
+            'handlers': ['console', 'syslog', 'mail_admins', 'sentry'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
 
 ALLOWED_HOSTS = [".ngrok.io", ".herokuapps.com"]
 
