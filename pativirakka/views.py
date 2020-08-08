a553c3f0baf186736d93f29e83f988b3e5106e1b
shyamkumar yadav <shyamkumaryadav2003@gmail.com>
2020-08-08 17:52:46 +0530

    fix Bug 101010101

--- pativirakka/views.py
+++ pativirakka/views.py
@@ -153,7 +153,7 @@ Reddit & GitHub 🌱 & telegram: @shyamkumaryadav```\n\n\n""")
                     if x:
                         from faker import Faker
                         fake = Faker()
-                        req = requests.get(url=url, headers={'User-Agent': fake.user_agent(), 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'})
+                        req = requests.get(params={'__a' : 1 } ,url=url, headers={'User-Agent': fake.user_agent(), 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'Connection': 'keep-alive'})
                         data=bs.BeautifulSoup(req.content, 'html.parser')
                         print(data.find('title').text)
                         type_ = data.find('meta', {'name':'medium'})['content']
